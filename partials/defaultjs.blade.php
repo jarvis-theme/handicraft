05cd9ac4a72ef82c99b1f60052c9c35d5e66aa3b
yogaswara <gistha.yogaswara@gmail.com>
2016-01-22 15:28:08 +0700

    update link cdn requirejs

--- partials/defaultjs.blade.php
+++ partials/defaultjs.blade.php
@@ -1,3 +1,3 @@
 <!-- Default js -->
-<script data-main="{{url(dirTemaToko().'handicraft/assets/js/app-main')}}" src="{{url('js/require.js')}}"></script>	
+<script data-main="{{url(dirTemaToko().'handicraft/assets/js/app-main')}}" src="https://cdnjs.cloudflare.com/ajax/libs/require.js/2.1.22/require.min.js"></script>	
 
